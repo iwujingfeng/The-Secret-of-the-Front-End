5b095f597d0d0e1d9dc35af247d54cdf2c818589
iwujingfeng <iwujingfeng@163.com>
2019-03-24 19:18:53 +0800

    copy

--- code/scene/copy.js
+++ code/scene/copy.js
@@ -99,5 +99,84 @@
 }
 
 {
+    // JSON解析
+    const obj = {
+        name: 'jack',
+        arr: [1]
+    }
+    const newObj = JSON.parse(JSON.stringify(obj))
+    newObj.arr = [5]
+    // obj.arr = [5]
+    console.log('JSON', newObj, obj) // { name: 'jack', arr: [ 5 ] } { name: 'jack', arr: [ 1 ] }
 
+    var arr = [{
+        name: 'jack'
+    }]
+    const newArr = JSON.parse(JSON.stringify(arr))
+    newArr[0].name = 'mike'
+    // arr[0].name = 'mike'
+    console.log('JSON', newArr, arr) // [ { name: 'mike' } ] [ { name: 'jack' } ]
+}
+
+{
+    // 回头分析 递归实现深拷贝
+    function deepCopy(source) {
+        let target = Array.isArray(source) ? [] : {}
+        for (var k in source) {
+            if (typeof source[k] === 'object') {
+                target[k] = deepCopy(source[k])
+            } else {
+                target[k] = source[k]
+            }
+        }
+        return target
+    }
+}
+
+{
+    // slice()方法深拷贝
+    const arr = [1, {
+        name: 'jack'
+    }]
+    const newArr = arr.slice()
+    newArr[0] = 6
+    newArr[1].name = 'mike'
+    console.log('slice', newArr, arr) //[ 6, { name: 'mike' } ] [ 1, { name: 'mike' } ]
+}
+
+{
+    // concat()方法深拷贝
+    const arr = [1, {
+        name: 'jack'
+    }]
+    const newArr = arr.concat()
+    newArr[0] = 6
+    newArr[1].name = 'mike'
+    console.log('concat', newArr, arr) // [ 6, { name: 'mike' } ] [ 1, { name: 'mike' } ]
+}
+
+{
+    // 扩展运算符深拷贝
+    const arr = [1, {
+        name: 'jack'
+    }]
+    const newArr = [...arr]
+    newArr[0] = 6
+    newArr[1].name = 'mike'
+    console.log('扩展运算符', newArr, arr) // [ 6, { name: 'mike' } ] [ 1, { name: 'mike' } ]
+
+    const obj = {
+        name: 'jack',
+        age: {
+            a: 18
+        },
+        sex: [1]
+    }
+    const newObj = {
+        ...obj
+    }
+    newObj.name = 'mike'
+    newObj.age.a = 20
+    newObj.sex.push(2)
+    console.log('扩展运算符', newObj, obj) // { name: 'mike', age: { a: 20 }, sex: [ 1, 2 ] } { name: 'jack', age: { a: 20 }, sex: [ 1, 2 ] }
 }

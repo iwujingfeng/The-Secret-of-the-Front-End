faafa31acb7fb23d9e06c1d7ef79973da1ea6592
iwujingfeng <iwujingfeng@163.com>
2019-03-31 14:54:58 +0800

    copy

--- code/scene/copy.js
+++ code/scene/copy.js
@@ -1,9 +1,19 @@
 {
-    // 原始数据
+    // 基本数据类型赋值
+    let str = 'jack'
+    let newStr = str
+    console.log('基本数据类型赋值', str, newStr, str === newStr) // jack jack true
+    newStr = 'mike'
+    console.log('基本数据类型赋值', str, newStr, str === newStr) // jack mike false
+
+    // 引用数据类型赋值
     const arr = [1, 3, 5]
     const obj = {
         a: 1,
-        b: 2
+        b: 2,
+        d: {
+            e: 'jack'
+        }
     }
     const newArr = arr // 进行赋值
     const newObj = obj
@@ -11,39 +21,82 @@
     // 更改数据
     newArr[3] = 7
     newObj.c = 3
+    newObj.d.e = 'mike'
 
-    console.log('=', newArr, arr) // 值相同 原始数据也被更改
-    console.log('=', newObj, obj) // 值相同 原始数据也被更改
+    console.log('引用数据类型赋值', newArr, arr, newArr === arr) // [ 1, 3, 5, 7 ] [ 1, 3, 5, 7 ] true
+    console.log('引用数据类型赋值', newObj, obj)
+    // { a: 1, b: 2, d: { e: 'mike' }, c: 3 } { a: 1, b: 2, d: { e: 'mike' }, c: 3 }
 }
 
 {
-    // 遍历复制 深拷贝
-    const arr = [1, 3, 5, {
-        a: 6
-    }]
-    const obj = {
-        a: 1,
-        b: 2,
-        c: {
-            d: 4
-        }
-    }
-    // 遍历复制
+    // 第一层数据为基本数据类型 数组为例
+    const arr = [1, 3, 5]
     const newArr = []
     for (let i = 0; i < arr.length; i++) {
         newArr.push(arr[i])
+    }
+    console.log('浅拷贝-基本数据', newArr, arr, newArr === arr, newArr[1] === arr[1])
+    // [ 1, 3, 5 ] [ 1, 3, 5 ] false true
+
+    // 第一层数据为基本数据类型 对象为例
+    const obj = {
+        a: 1,
+        b: 2,
     }
     const newObj = {}
     for (let key in obj) {
         newObj[key] = obj[key]
     }
-    // 修改第一层数据 深拷贝
-    // arr[3] = 7
-    // obj.c = 3
-    newArr[3] = 7
+    console.log('浅拷贝-基本数据', newObj, obj, newObj === obj, newObj.a === obj.a)
+    // { a: 1, b: 2 } { a: 1, b: 2 } false true
     newObj.c = 3
-    console.log('深拷贝', newArr, arr) // [ 1, 3, 5, 7 ] [ 1, 3, 5, { a: 6 } ]
-    console.log('深拷贝', newObj, obj) // { a: 1, b: 2, c: 3 } { a: 1, b: 2, c: { d: 4 } }
+    console.log('浅拷贝-基本数据', newObj, obj)
+    // { a: 1, b: 2, c: 3 } { a: 1, b: 2 }
+
+    // 第一层数据为引用类型数据 数组为例
+    const arr1 = [1, {
+        a: 6
+    }, {
+        b: 7
+    }]
+    const newArr1 = []
+    for (let i = 0; i < arr1.length; i++) {
+        newArr1.push(arr1[i])
+    }
+    console.log('浅拷贝-数组', newArr1, arr1, newArr1 === arr1)
+    // [ 1, { a: 6 }, { b: 7 } ] [ 1, { a: 6 }, { b: 7 } ] false
+    newArr1[0] = 5
+    newArr1[1] = 7
+    newArr1[2].b = 9
+    console.log('浅拷贝-数组', newArr1, arr1, newArr1 === arr1)
+    // [ 5, 7, { b: 9 } ] [ 1, { a: 6 }, { b: 9 } ] false
+    console.log('浅拷贝-数组', newArr1[2].b === arr1[2].b) // true
+
+
+    // 第一层数据为引用类型数据 对象为例
+
+    const obj1 = {
+        a: 1,
+        c: {
+            d: 4
+        },
+        d: {
+            e: 6
+        }
+    }
+
+    const newObj1 = {}
+    for (let key in obj1) {
+        newObj1[key] = obj1[key]
+    }
+    console.log('浅拷贝-对象', newObj1, obj1, newObj1.c === obj1.c)
+    // { a: 1, c: { d: 4 }, d: { e: 6 } } { a: 1, c: { d: 4 }, d: { e: 6 } } true
+    newObj1.a = 2
+    newObj1.c = 3
+    newObj1.d.e = 8
+
+    console.log('浅拷贝-对象', newObj1, obj1)
+    // { a: 2, c: 3, d: { e: 8 } } { a: 1, c: { d: 4 }, d: { e: 8 } }
 }
 
 {
@@ -79,43 +132,34 @@
 {
     // Object.assign 浅拷贝
     const obj = {
-        a: 1,
-        b: 2,
-        c: {
-            d: 4
+        a: {
+            b: 4
         }
     }
-
     const newObj = Object.assign({}, obj)
-    // 修改第一层数据 深拷贝
-    newObj.a = 3
-    // obj.a = 3
-    console.log('Object.assign', newObj, obj) // { a: 3, b: 2, c: { d: 4 } } { a: 1, b: 2, c: { d: 4 } }
-
-    // 修改第二层数据 浅拷贝
-    newObj.c.d = 3
-    // obj.c.d = 3
-    console.log('Object.assign', newObj, obj) // { a: 3, b: 2, c: { d: 4 } } { a: 1, b: 2, c: { d: 4 } }
+    newObj.a.b = 6
+    newObj.c = 3
+    console.log('Object.assign', newObj, obj) // { a: { b: 6 }, c: 3 } { a: { b: 6 } }
 }
 
 {
     // JSON解析
     const obj = {
         name: 'jack',
-        arr: [1]
+        arr: [1, 2]
     }
     const newObj = JSON.parse(JSON.stringify(obj))
-    newObj.arr = [5]
-    // obj.arr = [5]
-    console.log('JSON', newObj, obj) // { name: 'jack', arr: [ 5 ] } { name: 'jack', arr: [ 1 ] }
+    newObj.name = 'mike'
+    newObj.arr[1] = 5
+    console.log('JSON-obj', newObj, obj)
+    // { name: 'mike', arr: [ 1, 5 ] } { name: 'jack', arr: [ 1, 2 ] }
 
     var arr = [{
         name: 'jack'
     }]
     const newArr = JSON.parse(JSON.stringify(arr))
     newArr[0].name = 'mike'
-    // arr[0].name = 'mike'
-    console.log('JSON', newArr, arr) // [ { name: 'mike' } ] [ { name: 'jack' } ]
+    console.log('JSON-arr', newArr, arr) // [ { name: 'mike' } ] [ { name: 'jack' } ]
 }
 
 {
